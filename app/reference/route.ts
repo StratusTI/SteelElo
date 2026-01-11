2884f177492b96fa4e08c314593538cf5b76c802
Gusttavo Castro <gusttavoleandro2023@gmail.com>
2026-01-10 21:25:37 -0300

    fix(docs): add typing in scalar doc route

--- app/reference/route.ts
+++ app/reference/route.ts
@@ -1,8 +1,7 @@
 import { ApiReference } from '@scalar/nextjs-api-reference'
-import type { HtmlRenderingConfiguration } from '@scalar/nextjs-api-reference'
 
-const config: Partial<HtmlRenderingConfiguration> = {
+const config = {
   url: 'http://localhost:3000/openapi.json',
-  theme: 'alternate',
+  theme: 'alternate' as const,
 }
 export const GET = ApiReference(config)
