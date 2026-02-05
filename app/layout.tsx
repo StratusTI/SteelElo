ebf71556199dd36fb0988e9d58824f037387cede
Gusttavo Castro <gusttavoleandro2023@gmail.com>
2026-02-05 15:40:56 -0300

    style: remove head tag in principal layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en' className={cn('dark', inter.variable)}>
-      <head></head>
       <body className='root antialiased bg-background h-screen'>
         <ThemeProvider>
           <QueryProvider>{children}</QueryProvider>
