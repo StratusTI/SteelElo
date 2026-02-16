b17a77745258cc90b382a8b2d6a743959420258f
Gusttavo Castro <gusttavoleandro2023@gmail.com>
2026-02-16 12:19:49 -0300

    style: add tw-merge in heading components

--- app/components/typography/heading/h4.tsx
+++ app/components/typography/heading/h4.tsx
@@ -1,10 +1,13 @@
+import { cn } from "@/lib/utils";
+
 interface H4Props {
   children: React.ReactNode;
+  className?: string;
 }
 
-export function H4({ children }: H4Props) {
+export function H4({ children, className }: H4Props) {
   return (
-    <h4 className='scroll-m-20 text-xl font-semibold tracking-tight text-primary'>
+    <h4 className={cn('scroll-m-20 text-xl font-semibold tracking-tight text-primary', className)}>
       {children}
     </h4>
   );
